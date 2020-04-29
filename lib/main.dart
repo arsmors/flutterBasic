4bad7cb204247213d2f27c4f986df5bf53e15e63
arsens <aa@aa.lv>
2020-04-29 20:16:02 +0300

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'location_list.dart';
+
 import 'package:flutter_app_test/location_detail.dart';
 import 'location_detail.dart';
 import 'mocks/mock_location.dart';
 import 'models/location.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
-  return runApp(MaterialApp(
-      home: LocationDetail(mockLocation)
-  ));
+  final mockLocations = MockLocation.fetchAll();
+
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
 }
--- lib/mocks/mock_location.dart
+++ lib/mocks/mock_location.dart
@@ -2,8 +2,8 @@ import 'package:flutter_app_test/models/location.dart';
 import 'package:flutter_app_test/models/location_fact.dart';
 
 class MockLocation extends Location {
-  static FetchAny() {
-    return Location(
+  static final List<Location> items = [
+    Location(
         name: "Location Name",
         url:
             "https://i.pinimg.com/originals/7c/db/f9/7cdbf912029809728864ebdfd01e916f.jpg",
@@ -16,6 +16,30 @@ class MockLocation extends Location {
               title: "How to get here",
               text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. "
                   "ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,")
-        ]);
+        ]),
+    Location(
+        name: "xxx xxxx",
+        url:
+            "https://cdn1-www.dogtime.com/assets/uploads/2020/02/affenhuahua-mixed-dog-breed-pictures-COVER-650x368.jpg",
+        facts: <LocationFact>[
+          LocationFact(
+              title: "xx",
+              text: "aaa"),
+          LocationFact(
+              title: "xx",
+              text: "aaa"),
+          LocationFact(
+              title: "xx",
+              text: "aaa"),
+        ]),
+  ];
+
+  static Location fetchAny() {
+    return MockLocation.items[0];
+  }
+
+  static List<Location> fetchAll() {
+    return MockLocation.items;
+
   }
 }
